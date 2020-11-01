f34f8a097a3cba80d5f3afb5b7410e4f047c05dd
MSI\moneo <jvr.moneo@gmail.com>
2020-11-01 12:10:24 +0100

    añadir rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PublicationsComponent } from './core/components/publications/publications.component';
+import { PublishComponent } from './core/components/publish/publish.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { LogoutComponent } from './shared/components/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PublicationsComponent },
+  { path: 'publish', component: PublishComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
